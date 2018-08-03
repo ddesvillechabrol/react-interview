06236a6f93acdeb2732f9125f714c9af9bea54ea
DemisB <demis.borsalino@mobeye-app.com>
2018-08-03 18:45:32 +0200

    Adjustment.

---
+++ src/MicromaniaMap.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export default class MicromaniaMap extends Component {
+
+  render() {
+    return null;
+  }
+}
