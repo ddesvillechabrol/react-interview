9cfad0747fdd1bd6adb35f9d87f6bccbf8aca8e5
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2018-08-13 12:00:20 +0200

    display all shop of json file

--- src/MicromaniaMap.js
+++ src/MicromaniaMap.js
@@ -2,38 +2,40 @@ import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 
+const markers = [{
+  "name": "Micromania PARIS (Ternes)",
+  "address": "1 ave Niel",
+  "zipcode": "75017",
+  "city": "PARIS (Ternes)",
+  "coords": {"lon": 2.294205, "lat": 48.878664}
+},
+{
+  "name": "Micromania MONTPARNASSE Rue de Rennes",
+  "address": "126. Rue de Rennes",
+  "zipcode": "75006",
+  "city": "Paris",
+  "coords": {"lon": 2.326222, "lat": 48.846652}
+}]
+
 export default class MicromaniaMap extends Component {
 
-/*getShopPositions() {
- *  var shopData = require('./fixtures/micromania.json');
- *  let shop;
- *  for(var i = 0; i < shopData.length; i++){
- *    shop = shopData[i];
- *    console.log("Name:" + shop.name + " address: " + shop.address)
- *  }
- *}
- */
-  state = {
-    "name": "Micromania PARIS (Ternes)",
-    "address": "1 ave Niel",
-    "zipcode": "75017",
-    "city": "PARIS (Ternes)",
-    "coords": {"lon": 2.294205, "lat": 48.878664}
-  }
-
   render() {
-    const position = [this.state.coords.lat, this.state.coords.lon];
+    const markers = require('./fixtures/micromania.json');
+    const shopMarkers = markers.map(marker => (
+      <Marker position={coordsToList(marker.coords)} key='{marker_${marker.name}}'>
+        <Popup>
+          {shopToStrings(marker)}
+        </Popup>
+      </Marker>
+    ));
+    const position = [48.878664, 2.294205]
     return (
-      <Map center={position} zoom="15">
+      <Map center={position} zoom="12">
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            {shopToStrings(this.state)}
-          </Popup>
-        </Marker>
+        {shopMarkers}
       </Map>
     );
   }
@@ -41,6 +43,12 @@ export default class MicromaniaMap extends Component {
 
 function shopToStrings(shopData){
   return (
-    <p>{shopData.name} <br /> {shopData.address}, {shopData.zipcode}, {shopData.city}</p>
+    <span>{shopData.name} <br /> {shopData.address}, {shopData.zipcode}, {shopData.city}</span>
+  )
+}
+
+function coordsToList(shopCoord){
+  return (
+    [shopCoord.lat, shopCoord.lon]
   )
 }
