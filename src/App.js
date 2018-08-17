7112b49e779ae2768ccdc913174b9d0be53185a8
Dimitri Desvillechabrol <d.desvillechabrol@gmail.com>
2018-08-17 02:50:45 +0200

    add list of selected shop and download CSV

--- src/App.js
+++ src/App.js
@@ -3,11 +3,67 @@ import MicromaniaMap from './MicromaniaMap';
 import List from './List';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    const markers = require('./fixtures/micromania.json');
+    /* add selected state to know if a marker is selected or not */
+    this.state = {
+      markers: markers.map((marker) => {
+        return {
+          ...marker,
+          selected: false,
+        }
+      }),
+      selectedList: [],
+    };
+  }
+
+  mapClick(index) {
+    const markers = this.state.markers.slice();
+    if (markers[index].selected) {
+      return;
+    }
+    const selectedList = this.state.selectedList.slice();
+    selectedList.push(index);
+    markers[index].selected = true;
+    this.setState({
+      markers: markers,
+      selectedList: selectedList,
+    })
+  }
+
+  selectedToInfo() {
+    /*transform selected index array to json entry*/
+    const { selectedList, markers } = this.state;
+    return selectedList.map((shop) => {
+      return markers[shop];
+    });
+  }
+
+  removeSelected(i) { 
+    /* Remove selected shop */
+    const selectedList = this.state.selectedList.slice();
+    const markers = this.state.markers.slice();
+    const markerIndex = selectedList[i];
+    markers[markerIndex].selected = false;
+    selectedList.splice(i, 1);
+    this.setState({
+      markers: markers,
+      selectedList: selectedList,
+    })
+  }
+
   render() {
     return (
       <div>
-        <MicromaniaMap/>
-        <List/>
+        <MicromaniaMap
+          markers={this.state.markers}
+          onClick={(index) => this.mapClick(index)}
+        />
+        <List 
+          selectedList={this.selectedToInfo()}
+          onClick={(index) => this.removeSelected(index)}
+        />
       </div>
     );
   }
--- src/MicromaniaMap.js
+++ src/MicromaniaMap.js
@@ -2,32 +2,40 @@ import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 
-const markers = [{
-  "name": "Micromania PARIS (Ternes)",
-  "address": "1 ave Niel",
-  "zipcode": "75017",
-  "city": "PARIS (Ternes)",
-  "coords": {"lon": 2.294205, "lat": 48.878664}
-},
-{
-  "name": "Micromania MONTPARNASSE Rue de Rennes",
-  "address": "126. Rue de Rennes",
-  "zipcode": "75006",
-  "city": "Paris",
-  "coords": {"lon": 2.326222, "lat": 48.846652}
-}]
-
 export default class MicromaniaMap extends Component {
 
+  shopToStrings(shopData){
+    return(
+      <span>{shopData.name} <br /> {shopData.address}, {shopData.zipcode}, {shopData.city}</span>
+    )
+  }
+
+  coordsToList(shopCoord){
+    return (
+      [shopCoord.lat, shopCoord.lon]
+    )
+  }
+
+  renderMarker(marker){
+    const { markers } = this.props;
+    return markers.map((marker, index) => {
+      const coord = this.coordsToList(marker.coords);
+      const text = this.shopToStrings(marker);
+      return (
+        <Marker
+          key={`marker_${marker.name}`}
+          position={coord}
+          onClick={() => this.props.onClick(index)}
+        >
+          <Popup>
+            {text}
+          </Popup>
+        </Marker>
+      )
+    })
+  }
+
   render() {
-    const markers = require('./fixtures/micromania.json');
-    const shopMarkers = markers.map(marker => (
-      <Marker position={coordsToList(marker.coords)} key='{marker_${marker.name}}'>
-        <Popup>
-          {shopToStrings(marker)}
-        </Popup>
-      </Marker>
-    ));
     const position = [48.878664, 2.294205]
     return (
       <Map center={position} zoom="12">
@@ -35,20 +43,8 @@ export default class MicromaniaMap extends Component {
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {shopMarkers}
+        {this.renderMarker()}
       </Map>
     );
   }
-}
-
-function shopToStrings(shopData){
-  return (
-    <span>{shopData.name} <br /> {shopData.address}, {shopData.zipcode}, {shopData.city}</span>
-  )
-}
-
-function coordsToList(shopCoord){
-  return (
-    [shopCoord.lat, shopCoord.lon]
-  )
 }
